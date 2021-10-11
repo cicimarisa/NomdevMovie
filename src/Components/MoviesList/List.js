b0984614000af83c4e8c818d453c19cc67e31edc
Cici Marisa <cicimarisahardianti@gmail.com>
2021-10-11 09:02:38 +0800

    Fix Image Not Found

--- src/Components/MoviesList/List.js
+++ src/Components/MoviesList/List.js
@@ -4,7 +4,7 @@ import "./List.scss"
 function List(props) {
     return (
         <li>
-            <img src={props.movie.poster_path && `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${props.movie.poster_path}`} alt="Movie Image" />
+            <img src={props.movie.poster_path ? `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${props.movie.poster_path}` : 'empty'} alt="Movie Image" onError={(e) => e.target.onerror == null && (e.target.style.display = 'none')} />
             <p> <span className="title">{props.movie.title}</span><br />
                 <span className="description">{props.movie.overview}</span>
             </p>
